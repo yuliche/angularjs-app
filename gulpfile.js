3cd0eecce79000e80e74834a6c9dfc2d391d13a3
Julia Cherepakhina <julia.cherepakhina@dataart.com>
2019-01-17 18:48:32 +0200

    Add changes

--- gulpfile.js
+++ gulpfile.js
@@ -18,62 +18,64 @@ const production = environments.production;
 /** load config file based on enviroment */
 const configFile = production() ? "./src/env/prod.js" : "./src/env/dev.js";
 
-gulp.task('lint', function() {
-  return gulp.src('./src/app/**/*.js')
-    .pipe(jshint())
-    .pipe(jshint.reporter('default'));
+gulp.task('lint', function () {
+    return gulp.src('./src/app/**/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
 });
 
-gulp.task('scripts', function(){
-	return gulp.src(['./src/assets/**/*.js',configFile])
-			.pipe(uglify())
-			.pipe(concat('vendor.min.js'))
-			.pipe(gulp.dest('./public/'));
+gulp.task('scripts', function () {
+    return gulp.src(['./src/assets/**/*.js', configFile])
+        .pipe(uglify())
+        .pipe(concat('vendor.min.js'))
+        .pipe(gulp.dest('./public/'));
 });
 
-gulp.task('browserify', function() {
+gulp.task('browserify', function () {
     // Grabs the app.js file
     return browserify('./src/app/app.module.js')
         // bundles it and creates a file called main.js
         .transform(babelify.configure({
             presets: ["es2015"]
-          }))
+        }))
         .bundle()
-        .pipe(plumber())
+        .on('error', function (err) {
+            console.log('Error : ' + err.message);
+        })
         .pipe(source('main.js'))
         .pipe(gulp.dest('./public/'));
 })
 
-gulp.task('scss', function() {
+gulp.task('scss', function () {
     gulp.src('./src/assets/scss/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./src/assets/stylesheets/'));
 });
 
-gulp.task('copy', ['browserify','scss'], function() {
-    gulp.src(['./src/**/*.html','./src/**/*.css'])
+gulp.task('copy', ['browserify', 'scss'], function () {
+    gulp.src(['./src/**/*.html', './src/**/*.css'])
         .pipe(gulp.dest('./public'))
-		.pipe(browserSync.stream())
+        .pipe(browserSync.stream())
 });
 
-gulp.task('build',['lint', 'scss', 'copy', 'scripts']);
+gulp.task('build', ['lint', 'scss', 'copy', 'scripts']);
 
-gulp.task('browser-sync', ['build'], function() {
+gulp.task('browser-sync', ['build'], function () {
     browserSync.init({
         server: {
             baseDir: "./public",
-			// The key is the url to match
-			// The value is which folder to serve (relative to your current working directory)
-			routes: {
-				"/bower_components": "bower_components",
-				"/node_modules": "node_modules"
-			}
+            // The key is the url to match
+            // The value is which folder to serve (relative to your current working directory)
+            routes: {
+                "/bower_components": "bower_components",
+                "/node_modules": "node_modules"
+            }
         },
-		browser:"chrome"
+        browser: "chrome"
     });
 });
 
-gulp.task('default', ['browser-sync'], function(){
-	gulp.watch("./src/**/*.*", ["build"]);
-	gulp.watch("./public/**/*.*").on('change', browserSync.reload);
+gulp.task('default', ['browser-sync'], function () {
+    gulp.watch("./src/**/*.*", ["build"]);
+    gulp.watch("./public/**/*.*").on('change', browserSync.reload);
 })
--- src/app/app.module.js
+++ src/app/app.module.js
@@ -1,6 +1,6 @@
 import {componentsModule} from './components/components.module';
 import {sharedModule} from './shared/shared.module';
-import uiRouter from 'angular-ui-router';
+import uiRouter from '@uirouter/angularjs';
 import ngAnimate from 'angular-animate';
 import ngMaterial from 'angular-material';
 import routesConfig from './app.routes';
